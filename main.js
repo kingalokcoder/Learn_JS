acbe501841df61c55205f7613e3f60c7123d9a4f
Alok <alokranjan5111998@gmail.com>
2022-02-12 20:19:13 +0530

    ternary operator

--- main.js
+++ main.js
@@ -1,23 +1,5 @@
-//conditional
+//Ternary
 
-//only values are matched not data type
-const x=10;
-
-if(x=='10')
-{
-    console.log("x is 10");
-}
-
-const y=20;
-if(y===10)//matches both data type and value
-{
-    console.log("y is 10");
-}
-else if(y<10)
-{
-    console.log("y is less than 10");
-}
-else
-{
-    console.log("y is 20");
-}
+const x=11;
+const y= x>10?'red':'blue';
+console.log(y);
