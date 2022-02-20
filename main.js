df46d3ca845e93cd4073ddafc67f10f27cd2eb74
Alok <alokranjan5111998@gmail.com>
2022-02-20 21:24:13 +0530

    class in js learnt

--- main.js
+++ main.js
@@ -1,40 +1,23 @@
-//object oriented in js
-//creating Persos constructor
-
-function Person(firstName ,lastName, age)
+class Person
 {
-    this.firstName=firstName;
-    this.lastName=lastName;
-    this.age=age;
-    // this.FullName = function()
-    // {
-    //     return `${this.firstName} ${this.lastName} `
-    // }
-    // this.Age= function()
-    // {
-    //     return this.age;
-    // }
-}
-//Use of prototype
-Person.prototype.FullName = function()
-{
-    return `${this.firstName} ${this.lastName} `
+    constructor(firstName,lastName,age)
+    {
+        this.firstName=firstName;
+        this.lastName=lastName;
+        this.age=age;
+    }
+    get Age()
+    {
+        return this.age;
+    }
+    get FullName()
+    {
+        return `${this.firstName} ${this.lastName}`;
+    }
 }
 
-Person.prototype.Age= function()
-{
-    return this.age;
-}
-
-//Instantiate objects
-
-const person1= new Person('Alok','Ranjan',23);
-const person2 = new Person('Ankesh','Raj',22);
+person1 =new Person('Alok','Ranjan',23);
+person2=new Person('Anjesh','Halwai',22);
 console.log(person1);
 console.log(person2);
-console.log(person1.lastName);
-console.log(person2.FullName());
-console.log(person1.Age());
-
-
-
+console.log(person1.age);
