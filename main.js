0ea6493d112537aba0bcdde2a12232bedfbe15d9
Alok <alokranjan5111998@gmail.com>
2022-02-21 20:20:06 +0530

    dom manipulation learnt

--- main.js
+++ main.js
@@ -1,12 +1,10 @@
-//selecting elements in java script
-//single element selecotr
-console.log(document.getElementById('my-form'));
-console.log(document.querySelector('.container'));
-console.log(document.querySelector('#my-form'));
+const ul =document.querySelector('.items');
+// ul.remove();
+console.log(ul);
+// ul.lastElementChild.remove();
+ul.firstElementChild.textContent='Gym';
+ul.children[1].textContent='Java Script';
+ul.lastElementChild.innerHTML='<h1>Holiday</h1>';
 
-//selecing multiple elements
-console.log(document.querySelectorAll('.item'));
-console.log(document.getElementsByClassName('.items'));
-console.log(document.getElementsByTagName('li'));
-
-//generally we use query selector and query selector all orget element by id
+const btn= document.querySelector('.btn');
+btn.style.background='red';
