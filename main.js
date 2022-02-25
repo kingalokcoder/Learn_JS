4f7476784375a217f7b59ae07e56b860f2264814
Alok <alokranjan5111998@gmail.com>
2022-02-25 19:11:41 +0530

    made a simple adding elements application

--- main.js
+++ main.js
@@ -1,16 +1,29 @@
-const btn= document.querySelector('.btn');
+const myForm =document.querySelector('#my-form');
+const inputName= document.querySelector('#name');
+const inputEmail =document.querySelector('#email');
+const msg= document.querySelector('.msg');
+const users = document.querySelector('#users');
 
-// btn.addEventListener('click',(e)=>{
-//     e.preventDefault();
-//     console.log('clicked');});
+myForm.addEventListener('submit',onSubmit);
 
-// btn.addEventListener('click',(e)=>{
-//     console.log(e.target.className);
-// });
 
-btn.addEventListener('click',(e)=>{
+function onSubmit(e)
+{
     e.preventDefault();
-    document.querySelector('#my-form').style.background='red';
-    // document.querySelector('body').classList.add('bg-dark');
-    document.querySelector('.items').lastElementChild.innerHTML='<h1>Hello</h1>'
-})
+    if(inputName.value==='' || inputEmail ==='')
+    {
+             msg.innerHTML='Please enter all fields';
+             msg.classList.add('error');
+          setTimeout(()=>msg.remove(),3000);   
+    }
+    else
+    {
+const li = document.createElement('li');
+li.appendChild(document.createTextNode(`${inputName.value} : ${inputEmail.value}`));
+   users.appendChild(li);
+
+   //clear the fields
+   inputName.value='';
+   inputEmail.value='';
+    }
+}
