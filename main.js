7052265e3e3405b666610737ec63352f20c4ec05
Alok <alokranjan5111998@gmail.com>
2022-02-25 18:27:07 +0530

    query selector learnt

--- main.js
+++ main.js
@@ -1,10 +1,16 @@
-const ul =document.querySelector('.items');
-// ul.remove();
-console.log(ul);
-// ul.lastElementChild.remove();
-ul.firstElementChild.textContent='Gym';
-ul.children[1].textContent='Java Script';
-ul.lastElementChild.innerHTML='<h1>Holiday</h1>';
-
 const btn= document.querySelector('.btn');
-btn.style.background='red';
+
+// btn.addEventListener('click',(e)=>{
+//     e.preventDefault();
+//     console.log('clicked');});
+
+// btn.addEventListener('click',(e)=>{
+//     console.log(e.target.className);
+// });
+
+btn.addEventListener('click',(e)=>{
+    e.preventDefault();
+    document.querySelector('#my-form').style.background='red';
+    // document.querySelector('body').classList.add('bg-dark');
+    document.querySelector('.items').lastElementChild.innerHTML='<h1>Hello</h1>'
+})
